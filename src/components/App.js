87955b1e21066c2ec218fe438bd20d03df458f22
ankitt01 <ankitrawat0102@gmail.com>
2021-05-12 23:43:40 +0530

    using async await for promises

--- src/components/App.js
+++ src/components/App.js
@@ -5,15 +5,15 @@ import React, { Component } from 'react';
 
 class App extends React.Component {
   state = {  }
-  onSearchSubmit(term) {
-    axios.get('https://api.unsplash.com/search/photos', {
+  async onSearchSubmit(term) {
+    const response = await axios.get('https://api.unsplash.com/search/photos', {
       params: {query: term}, 
       headers: {
         Authorization: 'Client-ID -uyLIs1pg5YXEUCWuE0GmWPXO0yuoazSBvqfTgOCZG8'
       }     
-    }).then((response) => {
-      console.log(response.data.results);
     })
+
+    console.log(response.data.results)
   }
   render() { 
     return ( 
